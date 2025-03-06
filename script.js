3650a7f86a1abebdb67858bcb16f80bc38324c4e
rohitisog <rohitisog@gmail.com>
2025-03-06 22:46:55 +0530

    simple color dom button task done

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-const colorName=document.getElementById("colorName");
-const createBtn=document.getElementById("createBtn");
-const buttonList=document.getElementById("buttonList");
+const colorName = document.getElementById("colorName");
+const createBtn = document.getElementById("createBtn");
+const buttonList = document.getElementById("buttonList");
+const generateColorBtn = document.getElementById("generateColorBtn");
 
-createBtn.addEventListener("click",()=>{
-    let color=colorName.value;
-    const colorBtn=document.createElement("button");
-    colorBtn.style.backgroundColor=color;
-    // colorBtn.style.color=w;
-    colorBtn.textContent=color;
-    buttonList.appendChild(colorBtn)
-})
+createBtn.addEventListener("click", () => {
+  let color = colorName.value;
+  const colorBtn = document.createElement("button");
+  colorBtn.style.backgroundColor = color;
+  // colorBtn.style.color=w;
+  colorBtn.textContent = color;
+  buttonList.appendChild(colorBtn);
+});
+
+let color = "abcdefghijklmnopqrstuvwxyz1234567890";
+let generateColor="";
+generateColorBtn.addEventListener("click", () => {
+  let randomColor;
+  for (let i = 1; i <= 6; i++) {
+    randomColor = color[Math.floor(Math.random()*color.length)];
+    generateColor += randomColor;
+}
+console.log(generateColor)
+
+  console.log(randomColor);
+  console.log(generateColor);
+
+  // let randomColor=color[Math.floor(Math.random()*color.length)];
+  alert(generateColor);
+  const colorBtn = document.createElement("button");
+  colorBtn.style.backgroundColor = `#${generateColor}`;
+  colorBtn.textContent = generateColor;
+  buttonList.appendChild(colorBtn);
+  generateColor=""
+});
